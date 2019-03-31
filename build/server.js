3efbce0d2d96d87b16a52369741ef3741f30fd64
Kyle Leighton <kleighton2@yahoo.com>
2019-03-31 00:31:32 -0700

    try 2

--- build/server.js
+++ build/server.js
@@ -14,10 +14,10 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../index.html'));
 });
  
-// app.listen(port, function (err) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         open('http://localhost:' + port);
-//     }
-// });
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        // open('http://localhost:' + port);
+    }
+});
